0ba9b12dcf9fb48ebe4ccd72b37f1b9cc1ae2c3f
ppa993 <ppa.993@gmail.com>
2015-07-25 12:04:46 +0700

    first upload

---
+++ WorldConnecting/WorldConnecting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorldConnecting.MvcApplication" Language="C#" %>
---
+++ WorldConnecting/WorldConnecting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorldConnecting.Startup))]
+namespace WorldConnecting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
