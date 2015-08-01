a08c3f1d349495346f7c8923d458dc6d8d62fae5
ppa993 <ppa.993@gmail.com>
2015-08-01 11:40:58 +0700

    move to new folder

---
+++ Source Code/WC/WC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult NewsFeed(string id)
+        {
+            if (id != null || id != "") ViewBag.id = id;
+            return View();
+        }
+
+        public ActionResult Profile(string id)
+        {
+            if (id != null || id != "") ViewBag.id = id;
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Forgot()
+        {
+            return View();
+        }
+    }
+}
