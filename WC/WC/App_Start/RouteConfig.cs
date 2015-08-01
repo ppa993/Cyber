287f68fc5dba9ff0707c957e32f0ec58dec3c54c
anpjam459 <tanan.an459@gmail.com>
2015-08-01 09:59:06 +0700

    fix chat button repsonsive

--- WC/WC/App_Start/RouteConfig.cs
+++ WC/WC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "NewFeeds", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "NewsFeed", id = UrlParameter.Optional }
             );
         }
     }
