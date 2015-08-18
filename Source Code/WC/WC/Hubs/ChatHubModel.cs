65667b76708ae605a7d53028e726351d393b7c30
anpjam459 <tanan.an459@gmail.com>
2015-08-18 09:32:16 +0700

    add model, core, script ChatBox

---
+++ Source Code/WC/WC/Hubs/ChatHubModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WC.Hubs
+{
+    public class MessageDetail
+    { 
+        public string UserId { get; set; } 
+        public string Message { get; set; } 
+    }
+    public class UserDetail
+    {
+        public string ConnectionId { get; set; }
+        public string UserId { get; set; }
+    }
+}
